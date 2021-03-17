8699bf48bcb7ffd681f7764e4f105d2b38a90ab6
Adam Twardowski <adamtwardowski1@interia.pl>
2021-03-17 22:28:01 +0100

    Koniec

--- PP_LAB_1/Program.cs
+++ PP_LAB_1/Program.cs
@@ -2,7 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Serialization.Formatters;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PP_LAB_1
@@ -13,13 +16,56 @@ namespace PP_LAB_1
         {
             RandomNumberGenerator rng = new RandomNumberGenerator(1);
             List<int> values = new List<int>();
+            List<int> weight = new List<int>();
+            List<int> packed = new List<int>();
+            int cost = 0;
+            int load = 0;
 
-            for (int i = 0; i < 10; i++)
-                values.Add(rng.nextInt(1, 29));
+            System.Console.WriteLine("Podaj ilosc przedmiotów: ");
+            int count = Convert.ToInt32(System.Console.ReadLine());
+            System.Console.WriteLine("Podaj maksymalną wagę przedmiotów: ");
+            int weightMax = Convert.ToInt32(System.Console.ReadLine());
+            System.Console.WriteLine("Podaj maksymalną wartosc przedmiotów: ");
+            int valueMax = Convert.ToInt32(System.Console.ReadLine());
+            System.Console.WriteLine("Podaj pojemność plecaka: ");
+            int capacity = Convert.ToInt32(System.Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                values.Add(rng.nextInt(1, valueMax));
+                System.Console.Write(i+1 + "\t");
+            }
+            for (int i = 0; i < count; i++)
+            {
+                weight.Add(rng.nextInt(1, weightMax));
+            }
+            System.Console.WriteLine();
+
+            foreach (int x in weight)
+            {
+                System.Console.Write(x + "\t");
+            }
+
+            System.Console.WriteLine();
+
             foreach (int x in values)
             {
-                System.Console.WriteLine(x);
+                System.Console.Write(x + "\t");
             }
+            System.Console.WriteLine("\n Wybrano przedmioty o numerach: ");
+
+            for (int i = 0; i < count; i++)
+            {
+                if (load + weight[i] <= capacity)
+                {
+                    load = load + weight[i];
+                    cost = cost + values[i];
+                    packed.Add(i + 1);
+                    System.Console.Write(i + 1 + "\t");     //dodać listę z przedmiotami
+                }
+            }
+            System.Console.WriteLine("\n Wartość wybranych przedmiotów wynosi: " + cost);
+            System.Console.WriteLine("Waga wybranych przedmiotów wynosi: " + load);
             System.Console.ReadLine();
         }
     }
